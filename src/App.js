384f327baaee7e490ca6e106c3ad1924db81cdf5
Mike <secti0n33@live.com>
2021-07-10 19:13:02 -0400

    Updated CSS to be easier to look at, while beginning to add resume and other nav options to the page. Unfinished version.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css'
 
-
 /**
  * Navigation
  */
@@ -11,8 +10,11 @@ function Nav(props)
     <div className={"nav"}>
       <ul>
         <li><a className={"nava"} href={"#home"}>Home</a></li>
+        <li><a className={"nava"} href={"#resume"}>Resumé</a></li>
+        <li><a classname={"nava"}>|</a></li>
         <li><a className={"nava"} href={"mailto:msanchezmbm@gmail.com"}>Email</a></li>
         <li><a className={"nava"} href={"https://www.linkedin.com/in/michael-sanchez-02282001/"}>LinkedIn</a></li>
+        <li><a className={"nava"} href={"https://github.com/DiscardStudio"}>GitHub</a></li>
       </ul>
     </div>
   );
@@ -33,26 +35,38 @@ function Info(props)
       </div>
       <div className={"parallax2"}>
         <div className={"home"} id="design">
-          <p classname={"design"} style={{fontSize: "30px"}}>Bring Your Ideas to Life</p>
+          <br/>
+          <p classname={"design"}>Bring Your Ideas to Life</p>
+          <br/>
         </div>
       </div>
       <div className={"create"}>
         <h1 className={"create"}>We Build it Together</h1>
       </div>
       <div className={"parallax3"} id={"contact"}>
-        <div className={"home2"}>
-          <h1></h1>
+        <div className={"home"}>
           <p>
             <p>Be a part of the process</p>
             <p>Every step of the way</p>
           </p>
         </div>
       </div>
+      <div className={"create"}>
+        <h1 className={"create"}>Resumé</h1>
+      </div>
+      <div classname={"home"} id="resume">
+        <div id="Education">
+
+        </div>
+        <div id="Work">
+          
+        </div>
+      </div>
     </div>
   );
 }
 
-class Page extends React.Component
+class WebPage extends React.Component
 {
   render() {
     return (
@@ -63,4 +77,4 @@ class Page extends React.Component
   );}
 }
 
-export default Page;
+export default WebPage;
