4f5142af97684c62182f5583d9f873828f799133
Mike <secti0n33@live.com>
2021-07-11 14:01:03 -0400

    Added Resume and Made Style Changed

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function Info(props)
       <div className={"parallax1"}>
         <div className={"home"} id="home">
           <h1><code>Michael Sanchez</code></h1>
-          <p><code>Freelance Web and App Developer</code><code className={"blinking"}> |</code></p>
+          <p><code>Freelance Web Developer</code><code className={"blinking"}> |</code></p>
         </div>
       </div>
       <div id={"create"}>
@@ -36,7 +36,7 @@ function Info(props)
       <div className={"parallax2"}>
         <div className={"home"} id="design">
           <br/>
-          <p classname={"design"}>Bring Your Ideas to Life</p>
+          <p className={"design"}>Bring Your Ideas To Life</p>
           <br/>
         </div>
       </div>
@@ -54,19 +54,78 @@ function Info(props)
       <div className={"create"}>
         <h1 className={"create"}>Resumé</h1>
       </div>
-      <div className={"home"} id="resume">
-        <div>
-          <h1>Education</h1>
-        </div>
-        <hr/>
-        <div>
-          <h1>Work Experience</h1>
-          <p>
-            <b>Stevens Tutor</b> - September 2020 to December 2020<br/>
-            Taught Basic Boolean Algebra and applications of logic gates<br/>
-            Challenged Students to think in new ways<br/>
-            Helped students learn to debug their own code<br/>
-          </p>
+      <div className={"parallax4"}>
+        <div className={"resu"} id="resume">
+          <div>
+            <p>
+              <h1>Skills</h1>
+              <h3>Programming Languages:</h3>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>Python</li>
+                <li>Java</li>
+                <li>C++</li>
+                <li>Scheme</li>
+                <li>OCaml</li>
+                <li>ARMv8</li>
+                <li>LEGv8</li>
+                <li>XML</li>
+              </ul>
+              <h3>Frameworks and Technologies:</h3>
+              <ul>
+                <li>ReactJS</li>
+                <li>Android Studio</li>
+                <li>React Native</li>
+                <li>Flask</li>
+              </ul>
+              <hr/>
+              <h1>Education</h1>
+              <h2>Stevens Institute of Technology</h2>
+              Expected Graduation May 2023<br/>
+              Bachelors of Science, Computer Science<br/>
+              Relevant Courses:<br/>
+              <ul>
+                <li>Intro to Web Programming</li>
+                <li>Intro to Computer Science</li>
+                <li>Data Structures</li>
+                <li>Algorithms</li>
+                <li>Discrete Math</li>
+                <li>Programming Languages</li>
+                <li>Automata and Computation</li>
+                <li>Theory of Computation</li>
+                <li>Calculus 1, 2, and 3</li>
+              </ul><br/>
+              
+                <h2>Pocono Mountain East High School</h2>
+                Graduated May 2019<br/>
+                Relevant Courses:<br/>
+                <ul>
+                  <li>Intro to Web Programming</li>
+                  <li>Intro to Java</li>
+                  <li>Advanced Java</li>
+                  <li>AP Computer Science A</li>
+                  <li>CS Independent Study</li>
+                </ul><br/>
+                Extra Curricular Activity:
+                <ul>
+                  <li>Co-Captain of the Chess Team 2018-2019</li>
+                  <li>Varsity Chair Chess player 2015-2019</li>
+                </ul>
+            </p>
+          </div>
+          <hr/>
+          <div>
+            <h1>Work Experience</h1>
+            <p>
+              <h2>Stevens Tutor</h2>
+              September 2020 to December 2020<br/>
+              -Taught Basic Boolean Algebra and applications of logic gates<br/>
+              -Challenged Students to think in new ways<br/>
+              -Helped students learn to debug their own code<br/>
+            </p>
+          </div>
         </div>
       </div>
     </div>
